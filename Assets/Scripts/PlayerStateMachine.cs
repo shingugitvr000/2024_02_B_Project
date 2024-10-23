1506c2b5aff24f98ca77d801944083eaf813560a
Shingugitvr000 <Shingugitvr000@gmail.com>
2024-10-23 10:52:28 +0900

    [Feat] AnimationManager 제작

--- Assets/Scripts/PlayerStateMachine.cs
+++ Assets/Scripts/PlayerStateMachine.cs
@@ -38,6 +38,12 @@ public class PlayerStateMachine : MonoBehaviour
     //새로운 상태로 전환 하는 메서드 
     public void TransitionToState(PlayerState newState)
     {
+        //현재 상태와 새로운 상태가 같은 타입 일 경우
+        if (currentState?.GetType() == newState.GetType())
+        {
+            return;                     //같은 타입이면 상태를 전환 하지 않고 리턴 
+        }
+
         //현재 상태가 존재한다면 Exit 메서드를 호출 
         currentState?.Exit();       //검사해서 호출 종료 (?)는 IF 조건 
 
